f8b50dea5053539a947eb0de8942d9388d5a2885
ifTNT <happyjohn369@gmail.com>
2020-03-28 22:39:44 +0800

    Camera component

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import ARView from '@/components/ar'
 import ListRoute from '@/components/list_route'
+import Camera from '@/components/camera'
 
 Vue.use(Router)
 
@@ -18,6 +19,11 @@ let router = new Router({
       path: '/ar_example',
       name: 'AR example',
       component: ARView
+    },
+    {
+      path: '/camera_test',
+      name: 'Camera test',
+      component: Camera
     }
   ]
 })
