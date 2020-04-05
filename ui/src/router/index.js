94a4ccbdfd75f345fb334afd13c93b4f215bddc9
KTony <ktfortest@gmail.com>
2020-04-05 17:52:17 +0800

    add timeLine dosent done yet

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import ARView from '@/components/ar'
 import ListRoute from '@/components/list_route'
 import Article from '@/components/article'
+import TimeLine from '@/components/timeline'
 
 Vue.use(Router)
 
@@ -24,7 +25,14 @@ let router = new Router({
       path: '/article',
       name: 'Article',
       component: Article
+    },
+    //==========test================
+    {
+      path: '/timeline',
+      name: 'feef',
+      component: TimeLine
     }
+    //=======end test===============
   ]
 })
 
