dbcb214e64c80d2d02b2b497f052aa36c8bd59fb
Yp1997 <a1051223@mail.nuk.edu.tw>
2020-05-10 23:43:51 +0800

    User profile

--- ui/src/components/profile.vue
+++ ui/src/components/profile.vue
@@ -1,51 +1,98 @@
 <template>
     <div class="profile">
-            <div class="circle"></div>
-            <!-- <img class="changePicture" src="static/media/camera.png"> -->
-                <div class="name">使用者</div>
-                <div class="hobbies">興趣</div>
-                <div class="description">形容自己的一句話</div>
+                <profilePicture :diameter="parentDiameter" :Id="Id"></profilePicture>
+                <!-- <div class="profilePricture"></div> -->
+                <div class="name">{{ nickName }}</div>
+                <div class="hobbies">{{ interest }}</div>
+                <div class="description">{{ intro }}</div>
             <button class="button" type="button" color="black">聊天室</button>
         </div>
 </template>
-
 <script>
+import axios from 'axios';
+import ProPic from "./profile_picture.vue";
+// GET /user/:id/info
+// 給定使用者ID，取得該使用者的個人資料(名字、興趣、一句話)
+// 輸入參數: id(使用者ID)
+// 輸出:
+//  nickName: 使用者暱稱
+//  interest: 使用者興趣
+//  intro: 使用者形容自己的一句話
+
+// 輸入:
+
+// GET /api/v1/user/test/info
+// 輸出:
+
+// {
+//     "ok": "true",
+//     "nickName": "王小明",
+//     "interest": "打籃球",
+//     "intro": "我就爛"
+// }
+
 export default {
     name: "friend_profile",
+    props:{
+        Id:{
+            type: String,
+            //預設為0px 避免父類別沒有傳入資料
+            default: "a123"
+        }
+    },
     data(){
         return{
-
+            nickName: "",
+            interest: "",
+            intro: "",
+            parentDiameter: "10em",
         };
+    },
+    components: {
+        profilePicture: ProPic
+    },
+    mounted(){
+        axios
+        .get(server.apiUrl("/user/"+this.Id+"/info"))
+        .then(
+            function(response){
+                if(response.data.ok === "true"){
+                    this.nickName = response.data.nickName;
+                    this.interest = response.data.interest;
+                    this.intro = response.data.intro;
+                }
+                else{
+                    console.log("not ok");
+                }
+            }.bind(this)
+        )
+        .catch(error => {
+            console.log(error);
+        });
     }
 };
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
+
+
 .profile{
     width: 80vw;
     height: 80vh;
-    background-color: rgba(0,0,0,0.4);
+    background-color: rgba(0,0,0,0.5);
     border-radius:50px;
-    position:fixed;
-    top:10vh;
+    position: fixed;
+    top:8vh;
     left:10vw;
-    /* text-align:center; */
-}
-.circle{
-    width: 23vw;
-    height: 23vw;
-    background:white;
-    border-radius: 50%;
-    position: fixed;
-    left:39vw;
-    top:18vh; 
-}
-.changePicture{
-    width: 3vh;
-    height: 3vh;
-    position: fixed;
-    left: 56vw;
-    top: 32vh;
+    text-align: center;
+    padding-top: 30px;
+    padding-bottom: 5px;
+
+    profilePicture{
+        dispaly: inline;
+        width: 10em;
+        height: 10em;
+    }
 }
 .name{
     color:white;
@@ -58,18 +105,18 @@ export default {
 .hobbies{
     font-size: 5vw;
     font-family: Microsoft JhengHei;
-    color:white;
-    position:fixed;
-    top:48vh;
-    left:20vw;
+    color: white;
+    position: fixed;
+    top: 48vh;
+    left: 20vw;
 }
 .description{
     font-size: 5vw;
     font-family: Microsoft JhengHei;
-    color:white;
-    position:fixed;
-    top:62vh;
-    left:20vw;
+    color: white;
+    position: fixed;
+    top: 62vh;
+    left: 20vw;
 }
 .button{
     background-color:#E8E8E8;
@@ -78,8 +125,8 @@ export default {
     padding:2vh 5vw;
     text-align:center;
     font-size:3vh;
-    position:fixed;
-    top:75vh;
-    left:37.5vw;
+    position: fixed;
+    top: 75vh;
+    left: 37.5vw;
 }
 </style>
