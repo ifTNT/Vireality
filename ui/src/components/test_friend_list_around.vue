3cf320de6b8669fb0479baa8408b643b7725f49e
ifTNT <iftnt1999@gmail.com>
2020-08-23 13:22:18 +0800

    Completed 3d transformation of friend list

--- ui/src/components/test_friend_list_around.vue
+++ ui/src/components/test_friend_list_around.vue
@@ -9,10 +9,28 @@
     </ul>
     <div class="center-mark"></div>
     <div class="indicator" :style="{'transform': `rotate(-${currentDir}rad)`}"></div>
-    <input type="range" class="slider" ref="slider" v-model="currentDir" />
+    <canvas ref="indicator3d" class="indicator3d"></canvas>
+    <div class="slider-container">
+      <label>Cur. Dir.</label>
+      <span>{{(currentDir/Math.PI*180).toFixed(4)}}deg</span>
+      <!--<input type="range" class="slider" ref="currentDir" v-model="currentDir" />-->
+    </div>
+    <div class="slider-container">
+      <label>Roll</label>
+      <input type="range" class="slider" ref="roll" v-model="roll" />
+    </div>
+    <div class="slider-container">
+      <label>Yaw</label>
+      <input type="range" class="slider" ref="yaw" v-model="yaw" />
+    </div>
+    <div class="slider-container">
+      <label>Pitch</label>
+      <input type="range" class="slider" ref="pitch" v-model="pitch" />
+    </div>
   </div>
 </template>
 <script>
+import * as THREE from "three";
 export default {
   name: "testFriendListAround",
   data() {
@@ -21,26 +39,98 @@ export default {
       display: [], // The elements that will be display.
       currentDir: 0, // Radius.
       FOV: Math.PI / 3, // The FoV that the friend will be displyed. (rad)
-      widthDisplay: 300 // The range of projected direction. (px)
+      widthDisplay: 300, // The range of projected direction. (px)
+      roll: 0,
+      yaw: 0,
+      pitch: 0,
+      threeObj: {
+        renderer: null,
+        camera: null,
+        scene: null,
+        axesHelper: null,
+        cellPhone: null
+      }
     };
   },
   mounted() {
+    // Initialize Three.js
+    const indicatorWidth = 300;
+    const indicatorHeight = 300;
+    this.threeObj.renderer = new THREE.WebGLRenderer({
+      canvas: this.$refs.indicator3d,
+      antialias: true,
+      alpha: true
+    });
+    this.threeObj.renderer.setPixelRatio(indicatorWidth / indicatorHeight);
+    this.threeObj.renderer.setSize(indicatorWidth, indicatorHeight);
+    this.$refs.indicator3d.width = indicatorWidth;
+    this.$refs.indicator3d.height = indicatorHeight;
+
+    // Setup basic components of Three.js
+    this.threeObj.camera = new THREE.PerspectiveCamera(
+      60, //Calcualted FoV.
+      indicatorWidth / indicatorHeight, //Aspect ratio
+      0.5, //Near plate
+      1100 //Far plate
+    );
+
+    this.threeObj.camera.position.set(0, 0, 1);
+
+    this.threeObj.scene = new THREE.Scene();
+
+    this.threeObj.axesHelper = new THREE.AxesHelper(0.5);
+    this.threeObj.axesHelper.position.set(0, 0, 0);
+
+    this.threeObj.cellPhone = new THREE.Mesh(
+      new THREE.CubeGeometry(0.2, 0.3, 0.05),
+      new THREE.MeshNormalMaterial()
+    );
+
+    this.threeObj.scene.add(this.threeObj.axesHelper);
+    this.threeObj.scene.add(this.threeObj.cellPhone);
+    this.threeObj.renderer.render(this.threeObj.scene, this.threeObj.camera);
+
     // Change the minimum and maximum value of the slider
-    this.$refs.slider.min = 0;
-    this.$refs.slider.max = 2 * Math.PI;
-    this.$refs.slider.step = 0.0001;
+    [
+      //this.$refs.currentDir,
+      this.$refs.roll,
+      this.$refs.yaw,
+      this.$refs.pitch
+    ].forEach(slider => {
+      slider.min = 0;
+      slider.max = 2 * Math.PI;
+      slider.step = 0.0001;
+    });
 
     // Display current direction and visiable lables
     this.updateDir();
   },
   watch: {
-    currentDir: function(newVal, oldVal) {
+    roll: function() {
+      this.updateDir();
+    },
+    yaw: function() {
+      this.updateDir();
+    },
+    pitch: function() {
       this.updateDir();
     }
   },
   methods: {
     // Display current direction and visiable lables
     updateDir() {
+      // Update 3D indicator
+      let newRotation = new THREE.Euler(this.pitch, this.roll, this.yaw, "XYZ");
+      this.threeObj.axesHelper.rotation.copy(newRotation);
+      this.threeObj.cellPhone.rotation.copy(newRotation);
+      this.threeObj.renderer.render(this.threeObj.scene, this.threeObj.camera);
+
+      // Calucalte new heading
+      let quaternion = new THREE.Quaternion();
+      // The rotation from global frame to local frame
+      quaternion.setFromEuler(newRotation);
+      this.currentDir = this.calculateHeading(quaternion);
+
       // Clear the displayed lables
       this.display = [];
       // Enumerate whole circle.
@@ -90,6 +180,46 @@ export default {
       projectedDir += this.widthDisplay / 2;
 
       return projectedDir;
+    },
+
+    // Calculate heading angle in XY-Plane from given quaternion.
+    calculateHeading(orientation) {
+      let primaryProbe = new THREE.Vector3(0, 0, -1);
+      let secondProbe = new THREE.Vector3(0, 1, 0);
+
+      // Calculate the probes direction in global frame
+      primaryProbe.applyQuaternion(orientation);
+      secondProbe.applyQuaternion(orientation);
+      console.log(primaryProbe, secondProbe);
+
+      // Project the probes to the global XY-Plane
+      primaryProbe = this.projVec2XY(primaryProbe);
+      secondProbe = this.projVec2XY(secondProbe);
+
+      // Decide which probe can discribe the current heading
+      let selectedProbe = new THREE.Vector3();
+      const threshold = 1/Math.sqrt(2);
+      if (primaryProbe.length() < threshold) {
+        selectedProbe.copy(secondProbe);
+      } else {
+        selectedProbe.copy(primaryProbe);
+      }
+
+      // Calculate the heading angle from selected probe
+      let headingAngle = Math.atan2(selectedProbe.y, selectedProbe.x);
+
+      // Convert the angle from [-pi, pi] to [0, 2pi]
+      if(headingAngle<0){
+        headingAngle += 2*Math.PI;
+      }
+
+      return headingAngle;
+    },
+
+    // Project the given three dimentional vector to XY-Plane
+    projVec2XY(testVec) {
+      testVec.z = 0;
+      return testVec;
     }
   }
 };
@@ -154,5 +284,17 @@ export default {
     border-top: solid transparent 0.5em;
     border-bottom: solid transparent 0.5em;
   }
+}
+
+.slider-container {
+  width: 20em;
+  display: flex;
+  justify-content: space-between;
+  margin: 0.5em 0;
+}
+
+.indicator3d {
+  box-sizing: border-box;
+  margin-bottom: 2em;
 }
 </style>
