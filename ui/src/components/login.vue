75b20a10f5464d42089a5f6d1d23c0908783a10d
love124356 <as463431@gmail.com>
2020-11-27 00:48:45 +0800

    login css

--- ui/src/components/login.vue
+++ ui/src/components/login.vue
@@ -1,25 +1,33 @@
 <template>
     <div class="login">
-        <img class="logo" src="/static/media/green_person.png" />
+        <logo class="logo" style=" margin-left: 0"></logo>
+        <!-- <img class="logo" src="/static/media/icons/android-chrome-192x192.webp.png" /> -->
         <form class="inputLogin">
-            <label for="feedback-user">帳號</label>
-            <input v-model="userId"  id="feedback-user"/>
-            <p v-if="state">
-                介於1~20個字元
-            </p>
-            <p v-else>
-                OK!
-            </p>
+            <div class="accountArea">
+                <label for="account">帳號</label>
+                <span v-if="state" class="noticeAccountErr">
+                    介於1~20個字元
+                </span>
+                <span v-else class="noticeAccountOK">
+                    OK!
+                </span>
+                <input v-model="userId"  id="account"/>
+            </div>
             
-            <label for="text-password">密碼</label>
-            <input type="password" id="text-password" aria-describedby="password-help-block" />
-            <p id="password-help-block">
-                介於4~20個字元{{errMassage}}
-            </p>
+                
+            
+            <div class="passwordArea">
+                <label for="password">密碼</label>
+                <span class="noticePassword" v-bind:style="{ color: errColor}">
+                    介於4~20個字元{{errMassage}}
+                </span>
+                <input type="password" id="password" />
+            </div>   
+            
         </form>
         <div class="statusButtons">
-            <button @click="register">註冊</button>
-            <button @click="login">登入</button>
+            <button class="registerBtn" @click="register">註冊</button>
+            <button class="loginBtn" @click="login">登入</button>
         </div>
     </div>
 </template>
@@ -29,7 +37,7 @@
 // import 'bootstrap-vue/dist/bootstrap-vue.css'
 // import BootstrapVue from 'bootstrap-vue'
 import axios from 'axios';
-
+import LOGO from "./loading.vue";
 // Vue.use(BootstrapVue)
 
 export default {
@@ -39,8 +47,12 @@ export default {
         userId: '',
         state:true,
         errMassage:"",
+        errColor: '#76a5af',
 
     };
+  },
+  components: {
+    logo: LOGO,
   },
   watch:{
     // 如果 `userId`有改變就會運行
@@ -50,37 +62,40 @@ export default {
 
   },
    methods:{
-
        register(){
            console.log("註冊")
            document.location.href = "/#/registration"
        },
        login(){
            console.log("登入")
-           const password = document.querySelector("#text-password").value
+           const password = document.querySelector("#password").value
            if(!this.state && (password.length > 3 && password.length < 21)){
             console.log("OKLOGIN")
-             axios.post(server.apiUrl("/user/login"),
-             {
-                 uid: this.userId,
-                 password: password
-             })
-             .then(
-                 function(response) {
-                     console.log(response.data);
-                     if(response.data.ok==='true'){
-                        this.errMassage = ""
-                        document.location.href = "/#/main"
-                     }
-                     else{
+            axios.post(server.apiUrl("/user/login"),
+            {
+                uid: this.userId,
+                password: password
+            })
+            .then(
+                function(response) {
+                    console.log(response.data);
+                    if(response.data.ok==='true'){
+                    this.errMassage = ""
+                    document.location.href = "/#/main"
+                    }
+                    else{
                         this.errMassage = ",密碼錯誤"
-                     }
-                 }.bind(this)
-             )
-             .catch(error => {
-                 console.log(error);
-             });
-           }
+                        //this.errColor = "red"
+                    }
+                }.bind(this)
+            )
+            .catch(error => {
+                console.log(error);
+            });
+          }
+        //   else{
+        //       this.errColor = "red"
+        //   }
             
         }
    }
@@ -92,11 +107,11 @@ export default {
     height: 45vw;
     /* position: fixed;
     top: 5vw; */
-    margin-top:15vw;
+    margin-top:10vw;
     /* margin-left: 23vw; */
     border-radius: 100%;
     /* background-color: brown; */
-    margin-bottom:5vw; 
+    /* margin-bottom:5vw;  */
 }
 .login{
 
@@ -107,8 +122,47 @@ export default {
 
 }
 .inputLogin{
-    margin-bottom:5vw; 
+    /* margin-bottom:5vw;  */
     width: 80vw;
 }
+#account,#password{
+    width:80vw;
+    margin-top: 2vh;
+}
+.passwordArea{
+    margin-top: 2vh;
+}
+span{
+    font-size: 0.7em;
+}
+.noticeAccountOK{
+    color: #b4a7d6;
+}
+label{
+    color:#45818e;
+}
+.noticeAccountErr{
+    color:#76a5af;
+}
+input{
+    border: 1px solid #45818e;
+    border-radius: 4px;
+    outline:none; 
+} 
+button{
+    border-radius: 4px;
+    margin: 4vw;
+    border: 1px solid #76a5af;
+    background-color:white;
+    outline:none; 
+}
+/* .registerBtn{
+    background-color:#d0e0e3;
+    border: 1px solid #d0e0e3;
 
+}
+.loginBtn{
+    background-color: #76a5af;
+    border: 1px solid #76a5af;
+} */
 </style>
