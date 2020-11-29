6390c7e4178bb2b9c7c6d5495a30340ecd84248d
ifTNT <iftnt1999@gmail.com>
2020-11-29 17:17:57 +0800

    Adjuect toolbar

--- server/models/user_schema.js
+++ server/models/user_schema.js
@@ -1,53 +1,53 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var UserSchema = new Schema({
-    user_id: {
-        type: String,
-        required: true,
-        unique: true
+  user_id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  nickname: {
+    type: String,
+    default: "匿名人士",
+  },
+  birthday: {
+    type: String,
+  },
+  interest: {
+    type: String,
+    default: "他很懶什麼都沒寫",
+  },
+  intro: {
+    type: String,
+    default: "他很懶什麼都沒寫",
+  },
+  face_id: [Number],
+  avator: {
+    type: String,
+    default: "https://i.imgur.com/oPYT6RD.png",
+  },
+  friend_list: [String], //放user_id
+  join_time: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    longitude: {
+      type: Number,
+      default: 0,
     },
-    password: {
-        type: String,
-        required: true,
+    latitude: {
+      type: Number,
+      default: 0,
     },
-    nickname: {
-        type: String,
-        default: '匿名人士',
-    },
-    birthday: {
-        type: String,
-    },
-    interest: {
-        type: String,
-        default: '他很懶甚麼都沒寫',
-    },
-    intro: {
-        type: String,
-        default: '他很懶甚麼都沒寫',
-    },
-    face_id: [Number],
-    avator: {
-        type: String,
-        default:"https://i.imgur.com/oPYT6RD.png"
-    },
-    friend_list: [String], //放user_id
-    join_time: {
-        type: Date,
-        required: true,
-    },
-    location: {
-        longitude: {
-            type: Number,
-            default: 0,
-        },
-        latitude: {
-            type: Number,
-            default: 0,
-        },
-    },
-    geohash:{
-        type:String,
-        default: 's00000000',
-    }
+  },
+  geohash: {
+    type: String,
+    default: "s00000000",
+  },
 });
 module.exports = mongoose.model("User", UserSchema);
