c5d19488e60ca92b01f7859f5d04ab9fa4a6aba6
KTony <ktfortest@gmail.com>
2020-11-30 10:07:34 +0800

    add firebase config in index.js

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -23,9 +23,38 @@ import Login from "@/components/login";
 import Loading from "@/components/loading";
 import Logout from "@/components/logout";
 
+import * as app from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/database'
+import 'firebase/storage'
+
 Vue.use(Router);
 
 const AppName = "Vireality";
+
+app.initializeApp(firebaseConfig)
+
+export const firebase = app
+export const db = app.firestore()
+export const storageRef = app.storage().ref()
+
+export const usersRef = db.collection('users')
+export const roomsRef = db.collection('chatRooms')
+
+export const filesRef = storageRef.child('files')
+
+export const dbTimestamp = firebase.firestore.FieldValue.serverTimestamp()
+export const deleteDbField = firebase.firestore.FieldValue.delete()
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnDO5kPUSJmMN413GXCjCpN-Ye5hnga7I",
+  authDomain: "vireality-chatroom.firebaseapp.com",
+  databaseURL: "https://vireality-chatroom.firebaseio.com",
+  projectId: "vireality-chatroom",
+  storageBucket: "vireality-chatroom.appspot.com",
+  messagingSenderId: "820242761275",
+  appId: "1:820242761275:web:d83f1ce817961b5c2c3e1e"
+};
 
 let router = new Router({
   routes: [{
